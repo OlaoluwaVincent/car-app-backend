2d5b8116f92680308e26e3df9a764528573111c0
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-02-27 22:31:30 +0100

    installing dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CarsModule } from './cars/cars.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CarsModule, AnalyticsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
